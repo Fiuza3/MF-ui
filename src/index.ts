aa742f57fda8cd76fff0511516acd850873cdd3c
Marcus Vinicius Fiúza <marcusvinifiuza739@gmail.com>
2026-05-22 12:18:46 -0300

    feat: export all tokens and components from index

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Tokens
+export { cores } from "./tokens/cores";
+export type { Cores } from "./tokens/cores";
+export { familias, pesos } from "./tokens/tipografia";
+
+// Componentes
+export { Logo, RATIO_AVANCO_LOGO, RATIO_ALTURA_LOGO, RATIO_PADDING_LOGO } from "./componentes/Logo";
+export type { LogoProps } from "./componentes/Logo";
+export { LogoAnimado } from "./componentes/LogoAnimado";
+export type { LogoAnimadoProps } from "./componentes/LogoAnimado";
+export { Navbar } from "./componentes/Navbar";
+export type { NavbarProps, NavLink } from "./componentes/Navbar";
+export { Footer } from "./componentes/Footer";
+export type { FooterProps, FooterLink } from "./componentes/Footer";
